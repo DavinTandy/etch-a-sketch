c416bce1b32dc576c866054e7615d69fa62c945a
DavinTandy <dav.tandy@gmail.com>
2024-06-17 19:06:59 +0700

    Change grid color when mouse hover over them

--- js/script.js
+++ js/script.js
@@ -4,10 +4,17 @@ const smallContainer = document.createElement("div");
 smallContainer.className = "small-container";
 smallContainer.style.width = `${800/16}px`;
 smallContainer.style.height = `${800/16}px`;
-smallContainer.style.backgroundColor = "black";
+smallContainer.style.backgroundColor = "#EBE9E1";
 smallContainer.style.margin = "0px";
 smallContainer.style.padding = "0px";
 
 for (let i = 0; i < 256; i++) {
     container.appendChild(smallContainer.cloneNode(true));
+}
+
+const allSmallContainer = document.getElementsByClassName("small-container");
+for (let i = 0; i < allSmallContainer.length; i++) {
+    allSmallContainer[i].addEventListener("mouseout", function(){
+        allSmallContainer[i].style.backgroundColor = "#2C3531";
+    })
 }
