0ca023865e100a33d1475119a87e1c1f61a4adb1
DavinTandy <dav.tandy@gmail.com>
2024-07-26 22:44:02 +0700

    Change grid's background colors on hover

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ const container = document.getElementById("container");
 
 for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
+    div.classList.add("grids")
     div.style.height = "50px";
     div.style.width = "50px";
     container.appendChild(div);
+}
+
+const grids = document.getElementsByClassName("grids");
+
+for (let i = 0; i < grids.length; i++) {
+    grids[i].addEventListener("mouseover", function(){grids[i].style.backgroundColor = "#2C3531"})
 }
