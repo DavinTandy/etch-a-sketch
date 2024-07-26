5a5f5c76570742c31e917d4ce18ff762786c8021
DavinTandy <dav.tandy@gmail.com>
2024-07-26 23:15:34 +0700

    Add slider range to change grid size

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
+const slider = document.getElementById("slider");
+const sliderValue = document.getElementById("slider-value");
 const container = document.getElementById("container");
+
+slider.addEventListener("input", function(){sliderValue.innerHTML = "Grid size: " + slider.value + "x" + slider.value});
 
 for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
@@ -11,5 +15,5 @@ for (let i = 0; i < 256; i++) {
 const grids = document.getElementsByClassName("grids");
 
 for (let i = 0; i < grids.length; i++) {
-    grids[i].addEventListener("mouseover", function(){grids[i].style.backgroundColor = "#2C3531"})
+    grids[i].addEventListener("mouseover", function(){grids[i].style.backgroundColor = "#2C3531"});
 }
